903806efad71e310f02826db342f42f4b84a4f09
Natalie Werve <nlwerve@gmail.com>
2019-02-25 21:48:09 -0800

    Completed exercise 1 and exercise 4

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -13,6 +13,20 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         // write code here
+        // V=πr^2h
+
+        double radius; // holds value of radius of cylinder
+        double height; // holds value of height of cylinder
+        double volume; // holds value of volume of cylinder
+
+        radius = 3.14;
+
+        height = 5;
+
+        volume = Math.PI * Math.pow(radius, 2) * height; // calculate volume of cylinder
+
+        System.out.println("The volume of a cylinder with a radius of " + radius + " and height of " + height + " equals " + volume + ".");
+
 
     }
 }
