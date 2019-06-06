61beb398f966c04622379df4cabe95f5e68502ff
Natalie Werve <nlwerve@gmail.com>
2019-06-05 20:56:55 -0700

    completed exception handling lab 7

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.exception_handling.labs;
 
+import java.util.Scanner;
+
 /**
  * Exception Handling Exercise 7:
  *
@@ -8,20 +10,35 @@ package labs_examples.exception_handling.labs;
  */
 
 public class Exercise_07{
-    public static void main(String[] args) {
-        Bus bus = new Bus();
-        BusRider rider = new BusRider("Bob", 65);
+    Scanner scanner = new Scanner(System.in);
 
-        try {
-            bus.buyBusTicket(rider);
-        } catch (busFullException){
-            System.out.println("Bus is full, no more tickets available.");
+    public static void main(String[] args) {
+        Exercise_07 run = new Exercise_07();
+        Bus bus = new Bus();
+
+        boolean addingRiders = true;
+        while (addingRiders) {
+            BusRider rider = run.initializeRider();
+            try {
+                run.buyBusTicket(bus, rider);
+            } catch (busFullException e){
+                System.out.println("Sorry, the bus is full, no more tickets available.");
+                break;
+            }
         }
     }
 
-    public void buyBusTicket(BusRider busRider) throws busFullException {
-        if (bus.getPassengers().size() < 40) {
-            bus.getPassengers().add(busRider);
+    public void buyBusTicket(Bus bus, BusRider rider) throws busFullException {
+        if (bus.getPassengers().size() < 12) {
+            bus.getPassengers().add(rider);
+            System.out.println("Good news, we have a ticket available for you!");
+                if (rider.getAge() >= 65){
+                    System.out.println("You qualify for a senior citizen discount.");
+                    System.out.println();
+                } else {
+                    System.out.println("You are purchasing a full price ticket.");
+                    System.out.println();
+                }
         } else {
             throw new busFullException();
         }
@@ -34,5 +51,14 @@ public class Exercise_07{
         }
     }
 
+    public BusRider initializeRider () {
+        System.out.println("Hello and welcome to the bus! What is your name?");
+        String name = scanner.next();
 
+        System.out.println("And what is your age?");
+        int age = scanner.nextInt();
+
+        BusRider rider = new BusRider(name, age);
+        return rider;
+    }
 }
