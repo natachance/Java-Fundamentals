eaf0ffc045942ac837df24ee6e93d8276a832df9
Natalie Werve <nlwerve@gmail.com>
2019-07-23 18:53:35 -0700

    completed I/O Lab 3

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -28,7 +28,45 @@ class ByteArrayInputStreamExample {
     }
 }
 
-class DataInputStreamExample {
+class BufferedStreamExample {
+    public static void main(String[] args) {
+        String filePath = "src/labs_examples/input_output/files/byte_data";
+
+        try(BufferedInputStream bufferedInput = new BufferedInputStream(new FileInputStream(filePath))) {
+            byte[] buffer = new byte[8];
+            int x = 0;
+            while ((x = bufferedInput.read(buffer)) != -1) {
+                System.out.println(new String(buffer, 0, x));
+            }
+        } catch (IOException exception){
+                exception.printStackTrace();
+            }
+        }
+    }
+
+class BufferedCharacterStreamExample {
+    public static void main(String[] args) {
+        String newFilePath = "src/labs_examples/input_output/files/BufferedWriterExample.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(newFilePath))){
+            String x = "This is a String.";
+            bw.write(x);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(newFilePath))){
+            String y;
+            while((y = br.readLine()) != null){
+                System.out.println(y);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class DataStreamExample {
     public static void main(String[] args) {
         String newfilePath = "src/labs_examples/input_output/files/DataInputStreamExampleFile";
 
@@ -36,10 +74,11 @@ class DataInputStreamExample {
         double b = 2.3;
         char c = 'x';
 
-        try(DataOutputStream output = new DataOutputStream(new FileOutputStream(newfilePath))){
+        try(DataOutputStream output = new DataOutputStream(new FileOutputStream(newfilePath))){ //why must have FileOutputStream too?
             output.writeInt(a);
             output.writeDouble(b);
             output.writeChar(c);
+
         } catch(IOException e){
             e.printStackTrace();
         }
