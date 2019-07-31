44f0746766373faffae3aadee90d772315dac536
Natalie Werve <nlwerve@gmail.com>
2019-07-30 21:56:56 -0700

    completed multi-threading lab 2

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,22 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class ThreadsLabTwo {
+    public static void main(String[] args) {
+        LabTwo example = new LabTwo("Example");
+
+    }
+}
+
+class LabTwo extends Thread {
+        public LabTwo (String name) {
+            super(name);
+            start();
+        }
+
+        @Override
+        public void run(){
+            System.out.println("This is thread: " + getName());
+        }
+    }
