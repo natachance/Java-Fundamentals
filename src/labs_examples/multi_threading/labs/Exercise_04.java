6512db49ac0a2ee44db7e3da54c9533a5254b0f7
Natalie Werve <nlwerve@gmail.com>
2019-08-02 08:44:54 -0700

    completed multi-threading lab 4

--- src/labs_examples/multi_threading/labs/Exercise_04.java
+++ src/labs_examples/multi_threading/labs/Exercise_04.java
@@ -7,24 +7,46 @@ package labs_examples.multi_threading.labs;
  *      working as expected
  */
 
+
+class Exercise_04 {
+    public static void main(String[] args) {
+        PrintMessage pm = new PrintMessage();
+        pm.print("This is a message.");
+
+        PrintMessage2 pm2 = new PrintMessage2();
+        ThreadedPrint t = new ThreadedPrint("This is another message.", pm2);
+        ThreadedPrint t2 = new ThreadedPrint("This is yet another message.", pm2);
+        t.start();
+        t2.start();
+    }
+}
+
 class PrintMessage {
-    public void printMessage(String message){
+    public synchronized void print(String message){
         System.out.println(message);
     }
 }
 
-class ThreadedPrintMessage extends Thread {
-    private String message;
-    private Thread thread;
-    PrintMessage printMessage;
+class PrintMessage2 {
+    public void print(String message){
+        System.out.println(message);
+    }
+}
 
-    public void ThreadedPrint(String message, PrintMessage object){
-        message = message;
-        object = object;
+class ThreadedPrint extends Thread {
+    private String msg;
+    private Thread thread;
+    PrintMessage2 pm2;
+
+    ThreadedPrint(String message, PrintMessage2 object){
+        msg = message;
+        pm2 = object;
     }
 
     public void run(){
-        synchronized (printMessage){}
+        synchronized (pm2){
+            pm2.print(msg);
+        }
     }
 }
 
