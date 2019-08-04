8fa57920845e1f7b317bf64460a88f48623ef2d0
Natalie Werve <nlwerve@gmail.com>
2019-08-04 08:38:53 -0700

    started on multi-threading lab 5

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -5,3 +5,27 @@ package labs_examples.multi_threading.labs;
  *
  *      Demonstrate the use of a wait() and notify()
  */
+
+class Exercise_05 {
+    public static void main(String[] args) {
+    PrintMessages pm = new PrintMessages();
+    pm.printMessages();
+    }
+}
+
+class PrintMessages {
+    public void printMessages(){
+        boolean print = true;
+        while (!print) {
+            try {
+                System.out.println("Printing first message.");
+                wait();
+                System.out.println("Printing second message.");
+            } catch (InterruptedException ie) {
+                ie.getMessage();
+            }
+        }
+        print = false;
+        notify();
+    }
+}
