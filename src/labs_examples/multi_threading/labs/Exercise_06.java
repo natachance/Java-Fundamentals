7f49f03fc9e48d58c909f82eff642acf023261fd
Natalie Werve <nlwerve@gmail.com>
2019-08-06 18:05:52 -0700

    completed multi-threading labs

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -2,25 +2,24 @@ package labs_examples.multi_threading.labs;
 
 /**
  * Multithreading Exercise 6:
- *
- *      Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
- *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
+ * <p>
+ * Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
+ * print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
 
 // look at TickTock example
 // one class for odd and even printing methods
 // one class to set up the threads
-// once class to be controller with main method
-// NEED TO FIGURE OUT LOGIC - use something like if (x % 2 == 0) for evens, and if (x % 2 != 0) for odds?
-// or counter variable, x + 2?
-// currently only running each method once, not looping around again in spite of for loops w/ counters
+// one class to be controller with main method
+// use something like if (x % 2 == 0) for evens, and if (x % 2 != 0) for odds? or counter variable?
+
 
 class OddEven {
     String state;
     static int x = 1;
 
-    synchronized void odd (boolean running){
-        if(!running){
+    synchronized void odd(boolean running) {
+        if (!running) {
             state = "odd";
             notify();
             return;
@@ -35,31 +34,31 @@ class OddEven {
             while (!state.equals("even")) {
                 wait();
             }
-        } catch (InterruptedException exc){
+        } catch (InterruptedException exc) {
             System.out.println("Thread interrupted.");
-            }
         }
+    }
 
-        synchronized void even (boolean running){
-            if(!running){
-                state = "even";
-                notify();
-                return;
-            }
-
-            System.out.println(x);
-            x++;
-
+    synchronized void even(boolean running) {
+        if (!running) {
             state = "even";
             notify();
+            return;
+        }
 
-            try{
-                while(!state.equals("odd")){
-                    wait();
-                }
-            } catch (InterruptedException exc){
-                System.out.println("Thread interrupted.");
+        System.out.println(x);
+        x++;
+
+        state = "even";
+        notify();
+
+        try {
+            while (!state.equals("odd")) {
+                wait();
             }
+        } catch (InterruptedException exc) {
+            System.out.println("Thread interrupted.");
+        }
     }
 }
 
@@ -67,21 +66,23 @@ class CountingThread implements Runnable {
     Thread t;
     OddEven oddEven;
 
-    CountingThread (String name, OddEven oe){
+    CountingThread(String name, OddEven oe) {
         t = new Thread(this, name);
         oddEven = oe;
         t.start();
     }
 
-    public void run(){
-        if(t.getName().compareTo("odd") == 0) {
-            for(int i = 0; i <= 100; i++)
+    public void run() {
+        if (t.getName().compareTo("Thread 1") == 0) {
+            for (int i = 0; i < 50; i++) {
                 oddEven.odd(true);
-                oddEven.odd(false);
+            }
+            oddEven.odd(false);
         } else {
-                for(int i = 0; i <= 100; i++)
-                    oddEven.even(true);
-                    oddEven.even(false);
+            for (int i = 0; i < 50; i++) {
+                oddEven.even(true);
+            }
+            oddEven.even(false);
         }
     }
 }
