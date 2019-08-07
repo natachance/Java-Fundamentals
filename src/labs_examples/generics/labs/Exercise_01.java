1a65ee3fe15671cd8d361da8e0e130b977165138
Natalie Werve <nlwerve@gmail.com>
2019-08-07 09:49:10 -0700

    pulled updates and merging, also completed generics lab 1

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,37 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+
+class GenericsExample<T>{
+    private T val1;
+    private T val2;
+
+    public GenericsExample(T value, T value2) {
+        this.val1 = value;
+        this.val2 = value2;
+    }
+
+    public T getVal1() {
+        return val1;
+    }
+
+    public void setVal1(T val1) {
+        this.val1 = val1;
+    }
+
+    public T getVal2() {
+        return val2;
+    }
+
+    public void setVal2(T val2) {
+        this.val2 = val2;
+    }
+}
+
+class GenericsController{
+    public static void main(String[] args) {
+        GenericsExample<Integer> example1 = new GenericsExample(3, 5);
+        GenericsExample<String> example2 = new GenericsExample("horse", "emu");
+        GenericsExample<Double> example3 = new GenericsExample(2.6, 6.78);
+    }
+}
