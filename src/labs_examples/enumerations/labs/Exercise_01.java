52469caa058d93f0a119576ae09425c92bc763f6
Natalie Werve <nlwerve@gmail.com>
2019-08-11 11:01:33 -0700

    completed enumerations labs

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -3,8 +3,35 @@ package labs_examples.enumerations.labs;
 /**
  * Enumerations Exercise 1:
  *
- *      1) Create an enumeration. Use a controller class to demonstrate the use of the enum from a seperate class.
+ *      1) Create an enumeration. Use a controller class to demonstrate the use of the enum from a separate class.
  *      2) Create a parameterized enumeration that passes one or more values to it's constructor. Demonstrate the use
- *      of this enum from a seperate class.
+ *      of this enum from a separate class.
  */
 
+class EnumController{
+    public static void main(String[] args) {
+        System.out.println(size.MEDIUM);
+
+        System.out.println("The population of Canada is " + northAmericanCountries.CANADA.getPopulation() + " people.");
+    }
+}
+
+enum size{
+    EXTRASMALL, SMALL, MEDIUM, LARGE, EXTRALARGE;
+}
+
+enum northAmericanCountries{
+    CANADA("37 million"),
+    USA("327 million"),
+    MEXICO("129 million");
+
+    private String population;
+
+    northAmericanCountries(String population) {
+        this.population = population;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+}
