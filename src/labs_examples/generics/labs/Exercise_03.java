a5cdcca865b40d2f4ac795d203a36144a4710c57
Natalie Werve <nlwerve@gmail.com>
2019-08-11 08:17:14 -0700

    worked on generics lab 3

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,5 +1,8 @@
 package labs_examples.generics.labs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Generics Exercise 3:
  *
@@ -12,17 +15,44 @@ package labs_examples.generics.labs;
  *      3) Write a generic method to exchange the positions of two different elements in an array.
  *
  *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
- *
- *      5)
  */
 
+class GenericsExercise3Controller{
+    public static void main(String[] args) {
+        GenericsExercise3 ge3 = new GenericsExercise3();
+
+        System.out.println(ge3.genericSumMethod(1, 2.3));
+    }
+}
+
 class GenericsExercise3{
-//    public static <T extends Number, U extends Number> Double genericSumMethod(T t, U u){
-//        Double sum = 0.0;
-//        sum = (double) t + (double) u;
-//        return sum;
+    //#1 return sum of 2 arguments as a double
+    public static <T extends Number, U extends Number> Double genericSumMethod(T t, U u){
+        Double sum = 0.0;
+        sum = t.doubleValue() + u.doubleValue();
+        return sum;
+    }
+
+    //#2 count and return number of palindromes in collection of Strings
+    public static <E extends Collection> int palindromStrings(E list){
+        int numberOfStrings = 0;
+
+
+
+        return numberOfStrings;
+    }
+
+    //#3 switch positions of 2 elements in an array
+//    public static <E> E[] switchElements(E[] array){
+//
+//
 //    }
 
-
+    //#4 return largest element within range of a list
+//    public static <E extends Number> E findLargest(E[] array){
+//        E max;
+//
+////        return max;
+//    }
 
 }
